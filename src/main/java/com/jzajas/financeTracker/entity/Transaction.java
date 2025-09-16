7595dda87321eb29c58c6718bd57c8ac31315f1a
jzajas <jzajas1@gmail.com>
2025-09-16 19:22:27 +0200

    Dev/transaction (#1)
    * transaction model and repository creation
    
    * relation fix

---
+++ src/main/java/com/jzajas/financeTracker/entity/Transaction.java
@@ -0,0 +1,52 @@
+package com.jzajas.financeTracker.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Data
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private TransactionCategory category;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "created_at", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+}
---
+++ src/main/java/com/jzajas/financeTracker/entity/TransactionCategory.java
@@ -0,0 +1,13 @@
+package com.jzajas.financeTracker.entity;
+
+
+public enum TransactionCategory {
+    OUTSIDE_INCOME,
+    INSIDE_INCOME,
+    FOOD,
+    RECREATION,
+    MEDICAL,
+    TRANSPORT,
+    EDUCATION,
+    OTHER
+}
